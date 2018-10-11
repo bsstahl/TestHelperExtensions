99e35eacce02918898c90f0775a12858b2901c77
bsstahl <barry@bsstahl.com>
2018-10-10 19:36:28 -0700

    Add parameterized data to DateTimeExtensions_EqualWithinTolerance_Should.MatchADateIfAppropriate test.

--- tst/CBI.TestHelperExtensions.Test/DateTimeExtensions_EqualWithinTolerance_Should.cs
+++ tst/CBI.TestHelperExtensions.Test/DateTimeExtensions_EqualWithinTolerance_Should.cs
@@ -4,16 +4,19 @@ using TestHelperExtensions;
 
 namespace TestHelperExtensions.Test
 {
-    
+
     public class DateTimeExtensions_EqualWithinTolerance_Should
     {
 
         #region Parameterized Test
 
-        // TODO: Add remainig data from DateMatchingFromMinutes.xml file
-
         [Theory]
-        [InlineData("09/24/2015 17:24:05", "09/24/2015 17:25:04",0, 0, 1, 0, true)] // Matches a date less than 1 minute ahead if a 1 minute tolerance specified
+        [InlineData("09/24/2015 11:24:05", "09/24/2015 11:25:04", 0, 0, 1, 0, true)] // Matches a date less than 1 minute ahead if a 1 minute tolerance specified
+        [InlineData("09/24/2015 12:24:05", "09/24/2015 12:25:06", 0, 0, 1, 0, false)] // Does not match a date more than 1 minute ahead if a 1 minute tolerance specified
+        [InlineData("09/24/2015 13:24:05", "09/24/2015 13:23:06", 0, 0, 1, 0, true)] // Matches a date less than 1 minute behind if a 1 minute tolerance specified
+        [InlineData("09/24/2015 14:24:05", "09/24/2015 14:23:04", 0, 0, 1, 0, false)] // Does not match a date more than 1 minute behind if a 1 minute tolerance specified
+        [InlineData("09/24/2015 15:24:05", "09/24/2015 15:55:06", 0, 1, 0, 0, true)] // Matches a date less than 1 hour ahead if a 1 hour tolerance specified
+        [InlineData("09/24/2015 16:24:05", "09/24/2015 15:25:06", 0, 1, 0, 0, true)] // Matches a date less than 1 hour behind if a 1 hour tolerance specified
         public void MatchADateIfAppropriate(string targetDateString, string compareToDateString,
             Int32 days, Int32 hours, Int32 minutes, Int32 seconds, bool expected)
         {
